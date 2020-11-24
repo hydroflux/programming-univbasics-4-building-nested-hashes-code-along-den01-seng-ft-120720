9e259ed76f5dc9f43aa087118da2dd9c19491669
Jack Hubert <jphubert93@gmail.com>
2020-11-24 19:57:13 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,33 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     montague: {
-       patriarch: {
-         
-         
-       }
-       matriarch: {
-         
-       }
-       hero: {
-         
-       }
-       hero_friends: {
-         
-       }
-     }
-     capulate: {
-       patriarch: {
-         
-       }
-       matriarch: {
-         
-       }
-       heroine: {
-         
-       }
-       heroine_friends: {
-         
-       }
-     }
+     montague: {},
+     capulet: {}
    }
 end
